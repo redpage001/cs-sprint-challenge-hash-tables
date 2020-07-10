5e360fee1514ce2311976879a8455428ead40efb
Manuel De La Mora <Manuel@defaults-mbp.attlocal.net>
2020-07-10 10:59:52 -0700

    Completed all exercises for the Sprint Challenge.

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -3,6 +3,18 @@ def intersection(arrays):
     YOUR CODE HERE
     """
     # Your code here
+    # 0(n^2)
+
+    cache = dict()
+
+    for arr in arrays:
+        for num in arr:
+            if num in cache:
+                cache[num] += 1
+            else:
+                cache[num] = 1
+    # With the counter set, if the number has a count equal to the len of the array then it means it is in all the arrays, getting the value we need
+    result = [count[0] for count in cache.items() if count[1] == len(arrays)]
 
     return result
 
